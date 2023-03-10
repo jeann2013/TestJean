b4a0b362929ab50fb87eb9d5ad9563ecb9fcdb08
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-03-10 16:23:51 -0500

    add

---
+++ ModuleJean/Setup/Patch/Data/InitDataTestJean.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+namespace ModuleJean\ModuleJean\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+/**
+ * Class EnableDirectiveParsing
+ * @package Magento\Catalog\Setup\Patch
+ */
+class InitDataTestJean implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * PatchInitial constructor.
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        $data = [
+          ['name' => 'jean', 'email' => 'jean@jean.com'],
+          ['name' => 'jean2', 'email' => 'jean2@jean.com'],
+        ];
+        $this->moduleDataSetup->getConnection()->insertArray(
+            $this->moduleDataSetup->getTable('test_table'),
+            ['jean', 'jean@jean.com'],
+            $data
+        );
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
